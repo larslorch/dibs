8cef34b3b30fd2d0589825fbe0b7c61e2cd54cc7
Lars Lorch <lars.lorch@gmail.com>
2022-09-26 09:05:41 +0200

    updated pypi version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='dibs-lib',
-    version='1.1.0',
+    version='1.2.0',
     description='DiBS: Differentiable Bayesian Structure Learning',
     author='Lars Lorch',
     author_email='lars.lorch@inf.ethz.ch',
@@ -13,7 +13,7 @@ setup(
     ],
     packages=find_packages(),
     install_requires=[
-        'jax>=0.2.8',
+        'jax>=0.2.25',
         'jaxlib>=0.1.59',
         'numpy',
         'pandas',
